d6c2c79ffa17543a14d3fecb7bacf08ce8ab2903
SANDHILLS\LH218-10 <jad.talbert@gmail.com>
2026-01-21 11:38:03 -0500

    my first java program

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,1 +1,17 @@
+ /*
+@author: DeAndre Colston
+@date: 1/21/2026
+@purpose: My first Java Program
 
+CSC_151_DeAndreColston
+*/
+
+
+//this is my class name
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+    }
+}
