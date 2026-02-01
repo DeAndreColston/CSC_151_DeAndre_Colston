c79819681a7ba85c6c8b8019fca51877e0eb7cdc
DeAndreColston <colstonda048@mail.sandhills.edu>
2026-02-01 14:04:16 -0500

    Mammal Code

---
+++ src/labs/example/mammals/Mammal.java
@@ -0,0 +1,42 @@
+// CSC_151_DeAndre_Colston 
+// Building my first Mammal Class
+
+package labs.example.mammals;
+
+public class Mammal {
+    String hairColor;
+    String eyeColor;
+    double bodyTemp;
+
+    String agesString;
+    String weighString;
+    double height;
+
+public void run() {
+    System.out.println("The mammal is running.");
+}
+public void eat() {
+    System.out.println("The mammal is eating.");
+}
+public void sleep() {
+    System.out.println("The mammal is sleeping.");
+}
+public void scratch() {
+    System.out.println("The mammal is scratching.");
+}
+public void play() {
+    System.out.println("The mammal is playing.");
+}
+public void protect() {
+    System.out.println("The mammal is protecting.");
+}
+public void noise() {
+    System.out.println("The mammal is making noise.");
+}
+public void dig() {
+    System.out.println("The mammal is digging.");
+}
+public void smile() {
+    System.out.println("The mammal is smiling.");
+}
+}
