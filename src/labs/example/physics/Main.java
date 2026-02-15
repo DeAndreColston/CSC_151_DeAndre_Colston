670e86018490661cff7b0f303cc928031b0e0337
DeAndreColston <colstonda048@mail.sandhills.edu>
2026-02-15 18:30:39 -0500

    fixed committing

--- src/labs/example/physics/Main.java
+++ src/labs/example/physics/Main.java
@@ -5,12 +5,11 @@
  */
 package labs.example.physics;
 
-import java.text.DecimalFormat;
-
 public class Main {
 
     public static void main(String[] args) {
         Physics physics = new Physics();
+        
 
         double distance = physics.getDistance(3, 4);
         double velocity = physics.getVelocity(6, 7);
@@ -19,5 +18,12 @@ public class Main {
         double work = physics.getWork(7, 8);
         double KineticEnergy = physics.getKineticEnergy(10, 11);
         double PotentialEnergy = physics.getPotentialEnergy(12, 13, 14);
+        System.out.println("Distance: " + distance);
+        System.out.println("Velocity: " + velocity);
+        System.out.println("Momentum: " + momentum);
+        System.out.println("Force: " + force);
+        System.out.println("Work: " + work);
+        System.out.println("Kinetic Energy: " + KineticEnergy);
+        System.out.println("Potential Energy: " + PotentialEnergy);
     }
 }
--- src/labs/example/physics/Physics.java
+++ src/labs/example/physics/Physics.java
@@ -5,7 +5,6 @@
 */
 
 package labs.example.physics;
-import java.lang.Math;
 
 public class Physics {
     
@@ -16,32 +15,32 @@ public class Physics {
         return x;
     }
 
-    public static double getVelocity(double x, double t) {
+    public double getVelocity(double x, double t) {
         double v = x/t;
         return v;
     }
     
-    public static double getMomentum(double m, double v) {
+    public double getMomentum(double m, double v) {
         double p = m * v;
         return p;
     }
 
-    public static double getForce(double m, double a) {
+    public double getForce(double m, double a) {
         double f = m * a;
         return f;
     }
 
-    public static double getWork(double f, double d) {
+    public double getWork(double f, double d) {
         double w = f * d;
         return w;
     }
 
-    public static double getKineticEnergy(double m, double v) {
+    public double getKineticEnergy(double m, double v) {
         double ke = 0.5 * m * v* v;
         return ke;
     }
 
-    public static double getPotentialEnergy(double m, double g, double h) {
+    public double getPotentialEnergy(double m, double g, double h) {
         double pe = m * g * h;
         return pe;
     }
