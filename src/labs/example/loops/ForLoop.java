d43450c06af7bcd357df25b2d64bb8450dec2128
SANDHILLS\LH218-10 <jad.talbert@gmail.com>
2026-02-23 11:43:43 -0500

    fix later

---
+++ src/labs/example/loops/ForLoop.java
@@ -0,0 +1,24 @@
+/*
+@author: DeAndre Colston
+@date: 02/23/2026
+@purpose: Week 7 Lab
+*/
+
+package labs.example.loops;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        private static double executeWhileLoop() { 
+            int number = 1;
+            for (number = 1; number <= 20; number ++) {
+                System.out.println("number is" + number);
+            }
+        }
+        private static double sumTwoNumbers() {
+            int number = 1;
+            
+            for(number = 1; number <= 5; number ++)
+
+        }
+}
+}
---
+++ src/labs/example/loops/WhileLoop.java
@@ -0,0 +1,22 @@
+/*
+@author: DeAndre Colston
+@date: 02/23/2026
+@purpose: Week 7 Lab
+*/
+
+package labs.example.loops;
+
+public class WhileLoop {
+
+    private static void executeWhileLoop(String[] args)
+    {
+        int number = 1;
+        
+        while (number <= 10)
+        {
+            System.out.println("Hello");
+            number++;
+        }
+            System.out.println("That's all!");
+}
+}
