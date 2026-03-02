a68ecf5603a4a155689229a3cf77c5ad26cc2200
SANDHILLS\LH218-10 <jad.talbert@gmail.com>
2026-03-02 11:04:20 -0500

    week 8 lab

---
+++ src/labs/example/arrays/ArrayOperations.java
@@ -0,0 +1,29 @@
+/*
+@author: DeAndre Colston
+@date: 03/02/2026
+@purpose: Week 8 Lab
+*/
+
+package labs.example.arrays;
+
+public class ArrayOperations {
+    public static void main(String[] args) {
+
+        final int PLAYERS = 4;
+        int[] hours = new int[PLAYERS]
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Enter the hours played by " + PLAYERS + "players")
+
+        for (int index = 0; index < SPORTS; index++)
+        {
+            System.out.println("Sport " + (index + 1) + ": ")
+        }
+
+        public static void createNewArray(int a) {
+            int a;
+            return a;
+        }
+    }
+}
---
+++ src/labs/example/arrays/Main.java
@@ -0,0 +1,13 @@
+/*
+@author: DeAndre Colston
+@date: 03/02/2026
+@purpose: Week 8 Lab
+*/
+
+package labs.example.arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        createNewArray(3);
+    }
+}
