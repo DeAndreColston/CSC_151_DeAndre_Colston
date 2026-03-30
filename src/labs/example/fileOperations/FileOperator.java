f04f6df03ababe6c1a207f2a878a2e155caff903
DeAndre Colston <yourname@example.com>
2026-03-29 23:54:45 -0400

    week 11 lab

--- src/labs/example/fileOperations/FileOperator.java
+++ src/labs/example/fileOperations/FileOperator.java
@@ -4,15 +4,29 @@
 @purpose: Week 11 Lab
 */
 
+
 import java.io.*;
 import java.util.Arrays;
 
-public class FileOperations {
+public class FileOperator {
     public static void main(String[] args) {
         
         String csvFile = "src/labs/example/fileOperations/files/users.csv";
         String line = "";
         String cvsSplitBy = ",";
+
+        File errorLog = new File("src/labs/example/fileOperations/logs/csv_error.log");
+
+            try {
+        if (errorLog.createNewFile()) {
+        System.out.println("Success! csv_error.log has been created.");
+        } else {
+        System.out.println("The file already exists, so I didn't create a new one.");
+        }
+        } catch (IOException e) {
+        System.out.println("I couldn't create the file. Check if the 'logs' folder exists!");
+        e.printStackTrace();
+        }
 
         try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
             
